4117bdbec4b2157e91cdc24f7e53972998e8e5ed
aria1x1 <119283351+aria1x1@users.noreply.github.com>
2024-01-12 14:45:44 +0900

    Update 02_split_dataset.py

--- 이상 호흡 검출 Ai 모델/02_split_dataset.py
+++ 이상 호흡 검출 Ai 모델/02_split_dataset.py
@@ -2,20 +2,31 @@ import os
 import json
 import random
 import csv
-path_image = './SNU'
-path_json = './Json/Annotation.json'
+
+#Global variables
+path_image = './SNU' #참조할 데이터셋 이미지
+path_json = './Json/Annotation.json' #annotation이 포함된 json file
+
 def split_dataset(path1='./SNU', path2='./Json/Annotation-ver2.json', ratio = [0.00,0.00,1.0]):
+    #ratio는 dataset을 training, validation, test 파트로 나누어 리스트화한 것
+    #uft-8-sig로 csv 파일 형태의 Dataset summary를 작성
     f_summary = open(path1+'/Dataset_summary.csv', 'w', encoding='utf-8-sig', newline='')
     wr_summary = csv.writer(f_summary)
-    
+
+    #json_loader를 통해 가져온 annotation file에서 환자 번호와 AHI(무호흡-저호흡 지수)를 추출
     num_patient, lst_patient_AHI = json_loader(path2)
 
+    #key 변수를 통해 sorting function 정의 (AHI index)
     lst_patient_AHI = sorted(lst_patient_AHI, key = lambda lst_patient_AHI : lst_patient_AHI[2])
 
+    #ratio[0]은 training part의 비율, ratio[1]은 validation set의 비율, ratio[2]는 test set의 비율
     standard = [0, int(20*ratio[0]), int(20*(ratio[0]+ratio[1])), int(20*(ratio[0]+ratio[1]+ratio[2]))]
     standard2 = ratio
 
+    #ratio에 100을 곱해서 percentage로 만듦
     log = 'Dataset Ratio : [Train : {}%, Val : {}%, Test : {}%]'.format(ratio[0]*100, ratio[1]*100, ratio[2]*100) 
+    
+    #ratio 비율을 반영하여 Dataset_summary.csv 파일 아래에 summary 작성
     wr_summary.writerow([log])
     for fold in range(1, 11):
         trainset, valset, testset = [], [], []
@@ -110,6 +121,7 @@ def json_loader(json_path = './Json/Annotation2.json'):
         print('-----------AHI error -----')
     return num_of_patient, lst_patient_AHI            
 
+#위에서 정의한 split_dataset 호출
 if __name__ == "__main__":
     split_dataset()
 
