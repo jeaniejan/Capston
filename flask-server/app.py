57d277042a72b64751f5ebaf567264e015bfc2c3
wonjeong <wonj010101@gmail.com>
2024-01-19 11:42:18 +0900

    flask_server init

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,13 +1,9 @@
-from flask import Flask # Flask
+from flask import Flask
 
 app = Flask(__name__)
-
 @app.route('/users')
 def users():
-	# users 데이터를 Json 형식으로 반환한다
-    return {"members": [{ "id" : 1, "name" : "test1" },
-    					{ "id" : 2, "name" : "test2" }]}
-           
+    return {"members": [{"id": 1, "name": "test1"}, {"id": 2, "name": "test2"}]}
 
 if __name__ == "__main__":
     app.run('0.0.0.0', port=5000, debug=True)
